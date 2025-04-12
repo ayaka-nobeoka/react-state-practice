cb55404b9509be13b0c67a46dd62f3c5563301f7
ayaka-nobeoka <ys.training231106@gmail.com>
2025-04-12 18:46:06 +0900

    これから考えながらTODOアプリを作ってみる！

---
+++ state-demo-app/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <input></input>
+      <button>追加</button>
+
+      <button>削除</button>
+      <button>完了</button>
+    </div>
+  );
+}
+
+export default App;
