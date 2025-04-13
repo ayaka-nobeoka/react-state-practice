bdd16044302263b3b2d5dded9d168603b8d602d9
ayaka-nobeoka <ys.training231106@gmail.com>
2025-04-14 00:26:33 +0900

    4月13日

--- state-demo-app/src/App.js
+++ state-demo-app/src/App.js
@@ -1,15 +1,51 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <input></input>
-      <button>追加</button>
+  const [input, setInput] = useState("");
+  //input → 今の入力値（ユーザーが書いた内容）
+  //setInput → 入力値を更新する関数
+  const [items, setItems] = useState([]);
 
-      <button>削除</button>
-      <button>完了</button>
+  const handleChange = (e) => {
+    setInput(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const handleAdd = () => {
+    if (input.trim() === "") return;
+    setItems([...items, input]);
+    setInput("");
+  };
+
+  const handleDelete = () => {};
+
+  return (
+    <div>
+      <input type="text" value={input} onChange={handleChange}></input>
+      {/* 🎀 各記述の意味と役割
+　　　　✨ value={input}
+　　　　これは **「この入力欄の中の表示は、Reactの状態（input）で決めます」**という意味。
+　　　　つまり、見た目（DOM）を state がコントロールしてるということ。
+　　　　✨ onChange={handleChange}
+　　　　入力欄に何か変化があったら（文字が入力されたら）、
+
+　　　　handleChange を呼んで、Reactの状態（input）を更新する。 */}
+      <button onClick={handleAdd}>追加</button>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>
+            🐻 {item}
+            <button onClick={handleDelete}>削除</button>
+            <button>完了</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
 export default App;
+
+// 2つ目の setItems("") はいらないので 削除してOKです！
+// これがあると「配列」だった items が「文字列」になってしまってバグります💦
